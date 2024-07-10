2650c02142308ba7e0df6cecb760c55c142ce4fd
dkswoals <a4445656@gmail.com>
2024-07-10 20:38:57 +0900

    feat: change Board Object

--- practice_eddy/src/main/java/com/example/practice_eddy/model/DisposalBoardDTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.practice_eddy.model;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record DisposalBoardDTO(@NotBlank String itemName, @NotBlank String content) { }
