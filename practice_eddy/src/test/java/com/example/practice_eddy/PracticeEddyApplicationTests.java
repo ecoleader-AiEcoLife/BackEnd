7174ebd97150d5b843d72c18ce0e57a7f2e9818a
dkswoals <a4445656@gmail.com>
2024-07-10 12:20:52 +0900

    set up project

---
+++ practice_eddy/src/test/java/com/example/practice_eddy/PracticeEddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_eddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeEddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
